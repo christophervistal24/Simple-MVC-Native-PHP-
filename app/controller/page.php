77d8dc7c47b401b09cf862ac5b9c73201dfc5212
Christopher <christophervistal24@gmail.com>
2018-04-05 11:35:30 +0800

    Add Bootstrap

--- app/controller/page.php
+++ app/controller/page.php
@@ -3,11 +3,17 @@ class Page extends Controller
 {
     public function index()
     {
+        $data['title'] = 'Simple MVC | Home';
+        $this->views('includes/header',$data);
         $this->views('index',[]);
+        $this->views('includes/footer',[]);
     }
 
     public function login()
     {
-        $this->views('login',[]);
+       $data['title'] = 'Simple MVC | Login';
+       $this->views('includes/header',$data);
+       $this->views('login',[]);
+       $this->views('includes/footer',[]);
     }
 }
---
+++ app/views/includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $data['title'] ?></title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid">
